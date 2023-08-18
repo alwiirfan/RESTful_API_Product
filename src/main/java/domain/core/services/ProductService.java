0f691b61063156408cf186ac24238d970b5349eb
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-19 03:43:42 +0700

    feat: add Service for Product

---
+++ src/main/java/domain/core/services/ProductService.java
@@ -0,0 +1,40 @@
+package domain.core.services;
+
+import domain.core.models.entity.Product;
+import domain.core.models.repositorry.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // save = bisa untuk create dan untuk update
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product findByID(Long id){
+        Optional<Product> product = productRepository.findById(id);
+        return product.orElse(null);
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public void remove(Long id){
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> findByName(String name){
+        return productRepository.findByNameContains(name);
+    }
+}
