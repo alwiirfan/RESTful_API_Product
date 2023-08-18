7b80233f159b58d323bf4f9ce133fa1d62b92d86
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-18 13:25:15 +0700

    first commit to create restful api

---
+++ src/main/java/domain/core/controllers/WelcomeController.java
@@ -0,0 +1,16 @@
+package domain.core.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to Spring boot Rest API";
+    }
+
+}
