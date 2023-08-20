ce26a88108aaa54520a5bc463a68bd8c8a40a4ab
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-20 17:49:00 +0700

    feat: add controller of category

---
+++ src/main/java/domain/core/controllers/CategoryController.java
@@ -0,0 +1,9 @@
+package domain.core.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class CategoryController {
+}
