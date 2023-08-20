d2b0e53044ac3a2936727bf63276ccf561efc41a
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-20 17:45:07 +0700

    feat: add DTO for supplier

---
+++ src/main/java/domain/core/dto/SupplierDTO.java
@@ -0,0 +1,41 @@
+package domain.core.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public class SupplierDTO {
+
+    @NotEmpty(message = "Name is required")
+    private String name;
+
+    @NotEmpty(message = "Address is required")
+    private String address;
+
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Email is not valid")
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
