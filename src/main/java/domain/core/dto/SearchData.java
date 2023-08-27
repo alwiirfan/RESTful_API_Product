8e15fb143d4d8fc694ced6f0f0ad3f45258a7881
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-27 19:09:11 +0700

    feat: add DTO for Search Data

---
+++ src/main/java/domain/core/dto/SearchData.java
@@ -0,0 +1,14 @@
+package domain.core.dto;
+
+public class SearchData {
+
+    private String searchKey;
+
+    public String getSearchKey() {
+        return searchKey;
+    }
+
+    public void setSearchKey(String searchKey) {
+        this.searchKey = searchKey;
+    }
+}
