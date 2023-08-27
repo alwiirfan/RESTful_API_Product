75a342a31d9c5250c32d20f5126fcce35f773f02
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-27 19:00:00 +0700

    docs: add interface for findProductByName and findProductByNameLike

--- src/main/java/domain/core/models/repositorry/ProductRepository.java
+++ src/main/java/domain/core/models/repositorry/ProductRepository.java
@@ -1,11 +1,17 @@
 package domain.core.models.repositorry;
 
 import domain.core.models.entity.Product;
+import jakarta.websocket.server.PathParam;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-    List<Product> findByNameContains(String name); // unutuk mencari product berdasarkan nama product
+    @Query("SELECT p FROM Product p WHERE p.name = :name")
+    public Product findProductByName(@PathParam("name") String name); // unutuk mencari product berdasarkan nama product
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE :name")
+    public List<Product> findProductByNameLike(@PathParam("name") String name);
 }
