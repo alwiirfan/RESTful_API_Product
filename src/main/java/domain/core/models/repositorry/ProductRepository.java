08d57c54fb69b8f19dcd4d7c577b995d076f2ce7
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-29 19:34:52 +0700

    docs: add query custom to method repository findProductByCategory and findProductBySupplier

--- src/main/java/domain/core/models/repositorry/ProductRepository.java
+++ src/main/java/domain/core/models/repositorry/ProductRepository.java
@@ -1,6 +1,7 @@
 package domain.core.models.repositorry;
 
 import domain.core.models.entity.Product;
+import domain.core.models.entity.Supplier;
 import jakarta.websocket.server.PathParam;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -14,4 +15,10 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE p.name LIKE :name")
     public List<Product> findProductByNameLike(@PathParam("name") String name);
+
+    @Query("SELECT p FROM Product p WHERE p.category.id = :categoryId")
+    public List<Product> findProductByCategory(@PathParam("categoryId") Long categoryId);
+
+    @Query("SELECT p FROM Product p WHERE :supplier MEMBER OF p.suppliers")
+    public List<Product> findProductBySupplier(@PathParam("supplier") Supplier supplier);
 }
